6ee94dff3aba5059391dd58b99528d5b7afe7e47
Yudha Subki <yudhasubki@gmail.com>
2024-08-01 09:55:00 +0700

    feat: add passphrase for staging

--- cmd/release.go
+++ cmd/release.go
@@ -4,6 +4,7 @@ import (
 	"log/slog"
 	"os"
 	"os/exec"
+	"strings"
 
 	"github.com/spf13/cobra"
 )
@@ -16,15 +17,38 @@ func (r *Root) ReleaseCmd() *cobra.Command {
 		Run:   r.Release,
 	}
 
+	cmd.PersistentFlags().String("passphrase", "", "Passphrase only for production")
+	cmd.PersistentFlags().String("stage", "", "Stage production or staging")
+
 	return cmd
 }
 
 func (r *Root) Release(cmd *cobra.Command, args []string) {
+	stage, err := cmd.Flags().GetString("stage")
+	if err != nil {
+		slog.Error("error reading stage", "err", err)
+		return
+	}
+
+	var passphrase string
+	if stage == "production" {
+		passphrase, err = cmd.Flags().GetString("passphrase")
+		if err != nil {
+			slog.Error("error reading passphrase", "err", err)
+			return
+		}
+	}
+
 	for _, cmd := range r.ReleaseCommandList() {
+		slog.Info("start running command...", "cmd", cmd)
+
 		cmd := exec.Command("/bin/sh", "-c", cmd)
 		cmd.Stdout = os.Stdout
 		cmd.Stderr = os.Stderr
 
+		if passphrase != "" {
+			cmd.Stdin = strings.NewReader(passphrase)
+		}
 		err := cmd.Run()
 		if err != nil {
 			slog.Error("error executing the command", "cmd", cmd, "error", err)
@@ -38,8 +62,8 @@ func (r *Root) ReleaseCommandList() []string {
 		"set -e",
 		`echo "🛫 Deploying application"`,
 		"(sudo php artisan down) || true",
-		"git fetch origin main",
-		`git reset --hard origin/main`,
+		"sudo -S git fetch origin main",
+		`sudo -S git reset --hard origin/main`,
 		`sudo composer install --no-interaction --prefer-dist --optimize-autoloader --no-dev`,
 		`sudo php artisan migrate --force`,
 		`sudo php artisan optimize`,
@@ -51,6 +75,7 @@ func (r *Root) ReleaseCommandList() []string {
 		`sudo chgrp -R www-data ./storage ./bootstrap/cache`,
 		`sudo chmod -R ug+rwx ./storage ./bootstrap/cache`,
 		`sudo php artisan up`,
+		`sudo systemctl restart laravel_worker.service`,
 		`echo "🚀 Deployed application"`,
 	}
 }
